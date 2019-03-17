f1d1e5ad7d32b45e3c217009227ab3812f9e71f0
Mark Bukot <38723307+MarkMark25@users.noreply.github.com>
2019-03-17 18:01:19 +0800

    Update LoginController.php
    Username is used for logging in

--- nbi/app/Http/Controllers/Auth/LoginController.php
+++ nbi/app/Http/Controllers/Auth/LoginController.php
@@ -36,5 +36,9 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+    public function username(){
+        return 'username';
+    }
+
 
 }

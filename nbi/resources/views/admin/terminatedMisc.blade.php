546e3f1e824ded4e1b34bc55a7e00d6f45f4b17a
angelnica <32504332+angelnica@users.noreply.github.com>
2019-03-20 22:54:17 +0800

    Update Pages

---
+++ nbi/resources/views/admin/terminatedMisc.blade.php
@@ -0,0 +1,336 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>NBI-CAR</title>
+
+    <!-- Bootstrap core CSS-->
+    <link href="bower_components/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template-->
+    <link href="bower_components/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+
+    <!-- Page level plugin CSS-->
+    <link href="bower_components/vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="bower_components/css/sb-admin.css" rel="stylesheet">
+
+    <!--TAB IMAGE -->
+    <link rel="icon"  href="bower_components/image/nbi-logo.png">
+
+  </head>
+
+  <body id="page-top">
+
+    <nav class="navbar navbar-expand navbar-dark bg-dark static-top">
+
+      <a class="navbar-brand mr-1 " href="/">NBI-CAR</a>
+
+      <button class="btn btn-link btn-sm text-white order-1 order-sm-0" id="sidebarToggle" href="#">
+        <i class="fas fa-bars"></i>
+      </button>
+
+      <!-- Navbar Search -->
+      <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
+        <div class="input-group">
+          <!--
+          <input type="text" class="form-control" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
+          <div class="input-group-append">
+            <button class="btn btn-primary" type="button">
+              <i class="fas fa-search"></i>
+            </button>
+          </div>
+        -->
+        </div>
+      </form>
+
+      <!-- Navbar -->
+      <ul class="navbar-nav ml-auto ml-md-0">
+
+        <li class="nav-item dropdown no-arrow">
+          <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <i class="fas fa-user-circle fa-fw"></i>
+            <label name="UserName" id="UserName"> Mark Anthony</label> {{-- QUERY HERE --}}
+          </a>
+          <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
+            <a class="dropdown-item" href="/encoderProfile">Profile</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">Logout</a>
+          </div>
+        </li>
+      </ul>
+
+    </nav>
+
+    <div id="wrapper">
+
+ <!-- Sidebar -->
+ <ul class="sidebar navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="/adminHome">
+            <i class="fas fa-fw fa-home"></i>
+            <span>Home</span>
+          </a>
+        </li>
+       
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <i class="fas fa-fw fa-briefcase"></i>
+            <span>Manage Case</span>
+          </a>
+          <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+            <a class="dropdown-item" href="/adminCaseReport">Case Records</a>
+            <a class="dropdown-item" href="/adminCaseNature">Case Nature</a>
+          </div>
+        </li>
+
+        <li class="nav-item dropdown active">
+          <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <i class="fas fa-fw fa-briefcase"></i>
+            <span>Generate Report</span>
+          </a>
+          <div class="dropdown-menu" aria-labelledby="pagesDropdown">
+            <a class="dropdown-item" href="/adminAcmoCcnRequest">CCN & ACMO No. Req</a>
+            <a class="dropdown-item" href="/adminTransmittal">Transmittal</a>  
+            <a class="dropdown-item" href="/adminStatistics">Statistics</a>  
+            <a class="dropdown-item" href="/adminTerminatedCrimes">Terminated Crimes</a>  
+            <a class="dropdown-item" href="/adminTerminatedMisc">Terminated Misc...</a> 
+            <a class="dropdown-item" href="/adminPendingCrimes">Pending Crimes</a>  
+            <a class="dropdown-item" href="/adminPendingMisc">Pending Misc...</a>  
+            <a class="dropdown-item" href="/adminCaseReport2">Case Report</a>   
+          </div>
+        </li>
+       
+        <li class="nav-item">
+          <a class="nav-link" href="/adminManageAccount">
+            <i class="fas fa-fw fa-user-cog"></i>
+            <span>Manage Accounts</span></a>
+        </li>
+<br>
+<!-- Icon Cards-->
+<div class="content-wrapper">
+<div class="col">
+            <div class="row-xl-3 row-sm-6 mb-3">
+              <div class="card text-black o-hidden h-100">
+                <div class="card-body">
+                  <div class="card-body-icon">
+                    <i class="fas fa-fw fa-user-friends"></i>
+                  </div>
+                  <div class="mr-5"># active users</div>
+                </div>
+                <a class="card-footer text-white clearfix small z-1 bg-success" href="#">
+                  <span class="float-left" bg-primary>View Details</span>
+                  <span class="float-right">
+                    <i class="fas fa-angle-right"></i>
+                  </span>
+                </a>
+              </div>
+            </div>
+           
+            <div class="row-xl-3 ow-sm-6 mb-3">
+              <div class="card text-black o-hidden h-100">
+                <div class="card-body">
+                  <div class="card-body-icon">
+                    <i class="fas fa-fw fa-copy"></i>
+                  </div>
+                  <div class="mr-5"># closed cases</div>
+                </div>
+                <a class="card-footer text-white clearfix small z-1 bg-primary" href="#">
+                  <span class="float-left">View Details</span>
+                  <span class="float-right">
+                    <i class="fas fa-angle-right"></i>
+                  </span>
+                </a>
+              </div>
+            </div>
+            
+            <div class="row-xl-3 row-sm-6 mb-3">
+              <div class="card text-black  o-hidden h-100">
+                <div class="card-body">
+                  <div class="card-body-icon">
+                    <i class="fas fa-fw fa-download"></i>
+                  </div>
+                  <div class="mr-5">Total No. of Records</div>
+                </div>
+                <a class="card-footer text-white clearfix small z-1 bg-warning" href="#">
+                  <span class="float-left">View Details</span>
+                  <span class="float-right">
+                    <i class="fas fa-angle-right"></i>
+                  </span>
+                </a>
+              </div>
+            </div>
+          </div>
+    </ul>  
+<!-- /.container-fluid -->
+
+<div id="content-wrapper">
+<div class="container-fluid" style="padding-top:0%;padding-bottom:2%;">
+<div class="card mb-3">
+            <div class="card-header">
+            <center><i class="fas fa-user-clock"></i>
+                <font size="5">Terminated Miscellaneous Cases</font>
+            </center>
+            </div>
+          
+            <div class="card-body">
+            <button class="btn" href="/" style="position: absolute; right: 8%" data-target="#"><i class="fa fa-print"></i> Export to Excel</button>
+            <button class="btn" href="/" style="position: absolute; right: 1%" data-target="#"><i class="fa fa-file-excel"></i> Print</button>
+            
+           <!-- Dropdown for year -->
+        <b><span>Year</span></b>
+            <select name="year" id="year" style="width: 115px; height: 35px;">
+                            <option value="">Select year</option>
+                            <option value="2000">2000</option>
+                            <option value="2001">2001</option>
+                            <option value="2002">2002</option>
+                            <option value="2003">2003</option>
+                            <option value="2004">2004</option>
+                            <option value="2005">2005</option>
+                            <option value="2006">2006</option>
+                            <option value="2007">2007</option>
+                            <option value="2008">2008</option>
+                            <option value="2009">2009</option>
+                            <option value="2010">2010</option>
+                            <option value="2011">2011</option>
+                            <option value="2012">2012</option>
+                            <option value="2013">2013</option>
+                            <option value="2014">2014</option>
+                            <option value="2015">2015</option>
+                            <option value="2016">2016</option>
+                            <option value="2017">2017</option>
+                            <option value="2018">2018</option>
+                            <option value="2019">2019</option>
+            </select>
+
+        <!-- Dropdown for month -->
+        <b><span>Month</span></b>
+            <select name="month" id="month" style="width: 140px; height: 35px;">
+            <option value="">Choose month</option>
+                            <option value="january">January</option>
+                            <option value="february">February</option>
+                            <option value="march">March</option>
+                            <option value="april">April</option>
+                            <option value="may">May</option>
+                            <option value="june">June</option>
+                            <option value="july">July</option>
+                            <option value="august">August</option>
+                            <option value="september">September</option>
+                            <option value="october">October</option>
+                            <option value="november">November</option>
+                            <option value="december">December</option>
+            </select>
+      </div>
+           
+            <div class="card-body">
+              <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                  <thead>
+                  <tr>
+                        <th>No.</th>
+                        <th>NBI CCN No.ACMO No.</th>
+                        <th>CAR No.</th>
+                        <th>Nature</th>
+                        <th>Complainant/Victim</th>
+                        <th>Subject/s</th>
+                        <th>Date Assigned</th>
+                        <th>Date Terminated</th>
+                        <th>Agent-on-Case</th>
+                        <th>Status</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    @foreach($showData as $showData)
+                    <tr>
+                        <td>{{ $showData->number }}</td>
+                        <td>{{ $showData->ccnacmono }}</td>
+                        <td>{{ $showData->carno }}</td>
+                        <td>{{ $showData->nature }}</td>
+                        <td>{{ $showData->complainant }}</td>
+                        <td>{{ $showData->subjects }}</td>
+                        <td>{{ $showData->dateassigned }}</td>
+                        <td>{{ $showData->dateterminated }}</td>
+                        <td>{{ $showData->agent }}</td>
+                        <td>{{ $showData->status }}</td>
+                    </tr>
+                        @endforeach
+                    </tbody>
+                    </table>
+              </div>
+            </div>
+            
+          </div>
+          <b><span>TOTAL NO. OF RECEIVED AND TERMINATED CRIME CASES: </span></b> {{-- QUERY HERE --}}
+        </div>
+    </div>
+
+        </div>
+        <!-- /.container-fluid -->
+
+      <!-- Sticky Footer -->
+        <footer class="sticky-footer">
+          <div class="container my-auto">
+            <div class="copyright text-center my-auto">
+                <span>Copyright © eCaseRecord-NBI 2018-2019</span>
+            </div>
+          </div>
+        </footer>
+
+      </div>
+      <!-- /.content-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+      <i class="fas fa-angle-up"></i>
+    </a>
+
+    <!-- Logout Modal-->
+    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+            <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">×</span>
+            </button>
+          </div>
+          <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+          <div class="modal-footer">
+            <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+            <a class="btn btn-primary" href="">Logout</a><!--LINK HERE -->
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="bower_components/vendor/jquery/jquery.min.js"></script>
+    <script src="bower_components/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="bower_components/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Page level plugin JavaScript-->
+    <script src="bower_components/vendor/datatables/jquery.dataTables.js"></script>
+    <script src="bower_components/vendor/datatables/dataTables.bootstrap4.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="bower_components/js/sb-admin.min.js"></script>
+
+    <!-- Demo scripts for this page-->
+    <script src="bower_components/js/demo/datatables-demo.js"></script>
+
+  </body>
+
+</html>
